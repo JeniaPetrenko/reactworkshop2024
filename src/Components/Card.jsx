6bc41e68c523302fe5e8701ceae67f49589b102d
JeniaPetrenko <144125087+JeniaPetrenko@users.noreply.github.com>
2024-09-15 15:36:15 +0200

    first commit

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
+import React from "react";
 
-export default function Card({recipe}) {
-  // the repice prop sent from App.js is recived and deconstructed for easy use in component
+export default function Card({ recipe }) {
+  // the repice prop sent from App.js is recived and deconstructed for
+  //easy use in component
 
-  // hit F12, or right click page and select "inspect" to open developer tools in the broswer. Select "console" and look at the recipe object
-  console.log(recipe) // prints to the console
+  // hit F12, or right click page and select "inspect" to open developer
+  //tools in the broswer. Select "console" and look at the recipe object
+  console.log(recipe); // prints to the console
 
   // TODO
   // * complete the src-atribute on the img-tag to show the recipe picture
   // * complete the href-attribute #top on the link a-tag to the proper recipe url
   // * update the card with information you'd find interesting about the recipe
   // * update the card styling
-  
+
   return (
-    <div className='recipe-card'>
-      <img src="" alt={recipe.label} />
+    <div className="recipe-card">
+      <img src={recipe.image} alt={recipe.label} />
       <h2>{recipe.label}</h2>
-      <a href="#top" className='button'>To recipe</a>
+      <p>Calories: {recipe.calories || "N/A"}</p>
+      <p>Ingredient Count: {recipe.ingredientCount || "N/A"}</p>
+      <p>Total Time: {recipe.totalTime || "N/A"} minutes</p>
+      <p>Cooking Time: {recipe.cookingTime || "N/A"} minutes</p>
+      <p>Servings: {recipe.servings || "N/A"}</p>
+      <a href={recipe.url} className="button">
+        To recipe
+      </a>
     </div>
-  )
+  );
 }
